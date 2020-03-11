2bc121d4d91070529d07d1ddb107998457b5e18f
musergi <sergi.mus@gmail.com>
2020-03-11 19:49:28 +0100

    Hotfix

--- practica1/base_upgraded/src/TestReadLine.java
+++ practica1/base_upgraded/src/TestReadLine.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 
 public class TestReadLine {
     public static void main(String[] args) throws Exception {
-        Logger.getLogger("logger").setUseParentHandlers(false)
+        Logger.getLogger("logger").setUseParentHandlers(false);
         Logger.getLogger("logger").addHandler(new FileHandler("trace.log"));
         BufferedReader in = new EditableBufferedReader(
           new InputStreamReader(System.in));
