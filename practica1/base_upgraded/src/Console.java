5326eeec7c920ae4ad4a18972ae17e5ec09d9c93
musergi <sergi.mus@gmail.com>
2020-03-11 20:43:59 +0100

    Final commit

--- practica1/base_upgraded/src/Console.java
+++ practica1/base_upgraded/src/Console.java
@@ -4,12 +4,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Console implements PropertyChangeListener{
-    public static final Logger LOGGER = Logger.getLogger("logger");
-
     @Override
     public void propertyChange(PropertyChangeEvent e) {
         Action action = (Action) e.getNewValue();
-        LOGGER.log(Level.SEVERE, action.toString());
         System.out.print(action.actionString());
     }
 }
--- practica1/base_upgraded/src/TestReadLine.java
+++ practica1/base_upgraded/src/TestReadLine.java
@@ -4,9 +4,7 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 public class TestReadLine {
-    public static void main(String[] args) throws Exception {
-        Logger.getLogger("logger").setUseParentHandlers(false);
-        Logger.getLogger("logger").addHandler(new FileHandler("trace.log"));
+    public static void main(String[] args) {
         BufferedReader in = new EditableBufferedReader(
           new InputStreamReader(System.in));
         String str = null;
