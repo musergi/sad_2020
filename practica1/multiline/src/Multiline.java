48d90d95ee7d684876e7907d05cfb25478907ff2
musergi <sergi.mus@gmail.com>
2020-03-12 18:57:56 +0100

    Added basic utilities to multiline

--- practica1/multiline/src/Multiline.java
+++ practica1/multiline/src/Multiline.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Multiline {
     private int cursorRow, cursorColumn;
+    private List<StringBuilder> lines;
+
+    public Multiline() {
+        lines = new ArrayList<>();
+        lines.add(new StringBuilder());
+    }
+
+    public void process(int keyCode) {
+        addChar((char) keyCode);
+    }
+
+    public void addChar(char c) {
+        lines.get(cursorRow).append(c);
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Multiline((");
+        stringBuilder.append(cursorRow).append(", ").append(cursorColumn)
+        stringBuilder.append("), ");
+        stringBuilder.append(lines.get(0));
+        stringBuilder.append(")");
+        return stringBuilder.toString();
+    }
 }
