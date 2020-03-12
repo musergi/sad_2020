792c8fae75df77e8a63ce0318ef0370fdd22e217
musergi <sergi.mus@gmail.com>
2020-03-12 18:47:28 +0100

    Added unix compilation

--- practica1/multiline/build_c_mac.sh
+++ practica1/multiline/build_c_mac.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Edit java home to the proper path
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-13.0.2.jdk/Contents/Home
+
 gcc -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin src/ConsoleUtils.c -o bin/ConsoleUtils.o
 gcc bin/ConsoleUtils.o -shared -o bin/libconsole_utils.so
 rm bin/ConsoleUtils.o
--- practica1/multiline/src/Main.java
+++ practica1/multiline/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     private static void loadLibrary() {
         String mainFolder = new File(Main.class.getResource("Main.class").getFile()).getParent();
         System.load(mainFolder + "/libconsole_utils.so");
+        ConsoleUtils.init_console();
     }
     public static void main(String[] args) throws IOException {
         loadLibrary();
