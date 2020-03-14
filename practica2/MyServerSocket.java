c0e68cf2b3f6f5aa0bfb9dba1fe70205268e09a5
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-14 22:39:36 +0100

    Update MySocket

--- practica2/MyServerSocket.java
+++ practica2/MyServerSocket.java
@@ -1,7 +1,25 @@
 public class MyServerSocket{
     /**
+     * Servidor
      * funcionalment equivalent a la classe de Java ServerSocket 
      * però que encapsuli excepcions i els corresponents streams de text BufferedReader i PrintWriter. 
      * Aquesta classe haurà de disposar de mètodes de lectura/escriptura dels tipus bàsics.
      */
+    ServerSocket socket;
+
+    public MyServerSocket(){
+        //Create connection
+        serverSocket = new ServerSocket();
+        serverSocket.connection();
+        serverSocket.accept();
+    }
+    
+    public void sendMessage(final String message) {
+
+    }
+
+    public String receiveMessage() {
+        return null;
+
+    }
 }
--- practica2/MySocket.java
+++ practica2/MySocket.java
@@ -1,7 +1,41 @@
-public class MySocket{
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MySocket {
     /**
+     * Client
      * funcionalment equivalent a la classe de Java Socket  
      * però que encapsuli excepcions i els corresponents streams de text BufferedReader i PrintWriter. 
      * Aquesta classe haurà de disposar de mètodes de lectura/escriptura dels tipus bàsics.
      */
+    Socket socket;
+    BufferedReader buff; 
+    PrintWriter writer; 
+
+    public MySocket(SocketAddress address, PrintWriter writer){
+        //Create connection with the server
+        socket = new Socket();
+        socket.connect(address);
+
+        this.buff = new BufferedReader(socket.getInputStream());
+        this.writer = new PrintWriter(socket.getOutputStream());
+
+    }
+    /**
+     * Send a message to the client once the connection is made
+     * @param message The message that the client has to send
+     */
+    public void sendMessage(final String message) {
+        writer.write(message); //Lo que no se como enviarlo al otro puerto 
+        writer.flush();
+        writer.close();
+    }
+
+    public String receiveMessage() {
+        message = buff.readLine();
+        buff.close();
+        return message;
+    }
+
 }
