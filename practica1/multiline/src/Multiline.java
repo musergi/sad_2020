696cb4d43aa02622e8c0d00497df81ffdbe26518
musergi <sergi.mus@gmail.com>
2020-03-14 18:41:56 +0100

    Finished multiline

--- practica1/multiline/src/Multiline.java
+++ practica1/multiline/src/Multiline.java
@@ -54,71 +54,170 @@ public class Multiline {
     }
 
     public void addChar(char c) {
+        // Get the current line
         StringBuilder currentLine = lines.get(cursorRow);
+        
+        
         if (insert && cursorColumn < currentLine.length()) {
+            // If in insert mode and a char is in the cursor position
+            
+            // Replace cursor char by incoming char
             currentLine.setCharAt(cursorColumn, c);
         } else {
+            // Append char in specified position
             currentLine.insert(cursorColumn, c);
-            pcs.firePropertyChange("text", null, currentLine.substring(cursorColumn));
         }
+        
+        // Update text in view
+        pcs.firePropertyChange("text", null, currentLine.substring(cursorColumn));
+        
+        // Move cursor forward by 1
         cursorColumn++;
+        
+        // Update cursor view
         pcs.firePropertyChange("cursor", null, new CursorView.CursorDelta(0, 1));
     }
 
     public void moveCursorH(int delta) {
+        // Save cursor position for view update
+        int previousCursorRow = cursorRow;
+        int previousCursorColumn = cursorColumn;
+    
+        // Calculate new cursor position
         int newCursorColumn = cursorColumn + delta;
+        
         if (newCursorColumn == -1 && cursorRow > 0) {
+            // If the cursor is alredy at the start and has a line above
+            
+            // Go up one line
             cursorRow--;
+            // Go to the end of the previous line
             cursorColumn = lines.get(cursorRow).length();
         } else if (newCursorColumn == lines.get(cursorRow).length() + 1 && cursorRow < lines.size() - 1) {
+            // If the cursor is at the end of the line and has a line above
+            
+            // Move to the start of the next row
             cursorRow++;
             cursorColumn = 0;
         } else if (newCursorColumn >= 0 && newCursorColumn <= lines.get(cursorRow).length()) {
+            // If the new cursor column is in the current line boundries
+            
+            // Update cursor column
             cursorColumn = newCursorColumn;
         }
+        
+        // Update view
+        pcs.firePropertyChange("cursor", null, new CursorView.CursorDelta(
+            cursorRow - previousCursorRow,
+            cursorColumn - previousCursorColumn));
     }
 
     public void moveCursorV(int delta) {
+        // Save cursor position for view update
+        int previousCursorRow = cursorRow;
+        int previousCursorColumn = cursorColumn; 
+    
+        // Move cursor verticaly
         cursorRow = Math.min(Math.max(0, cursorRow + delta), lines.size() - 1);
-        int lineLength = lines.get(cursorRow).length();
-        if (cursorColumn > lineLength) {
-            cursorColumn = lineLength;
-        }
+        
+        // If new line is shorter move cursor to line end
+        cursorColumn = Math.min(cursorColumn, lines.get(cursorRow).length());
+        
+        // Update view
+        pcs.firePropertyChange("cursor", null, new CursorView.CursorDelta(
+            cursorRow - previousCursorRow,
+            cursorColumn - previousCursorColumn));
     }
 
     public void delete(boolean right) {
+        // Get current line
         StringBuilder currentLine =  lines.get(cursorRow);
+        
+        // Calculate deletion position
         int deletePosition = right ? cursorColumn : cursorColumn - 1;
         if (deletePosition >= 0 && deletePosition < currentLine.length()) {
-            currentLine.deleteCharAt(deletePosition);
+            // If in boundries
+            
+            // Move cursor if necesary
             moveCursorH(right ? 0 : -1);
+            
+            // Delte proper character
+            currentLine.deleteCharAt(deletePosition);
+            
+            // Update line end to fit new text
+            pcs.firePropertyChange("text", null, "\u001b[K" + currentLine.substring(cursorColumn));
         } else if (deletePosition == -1 && cursorRow > 0) {
+            // If at the start of row and has a row above
+
+            // Get previous line
             StringBuilder previousLine = lines.get(cursorRow - 1);
-            cursorColumn = previousLine.length();
-            previousLine.append(currentLine);
+            
+            // Remove line
             lines.remove(cursorRow);
+            // Update view to line removal
+            String[] movedLines = new String[lines.size() - cursorRow];
+            for (int i = 0; i < movedLines.length; i++) {
+                movedLines[i] = lines.get(cursorRow + i).toString();
+            }
+            pcs.firePropertyChange("text", null, "\u001b[J" + String.join("\n", movedLines));
+            
+            // Move cursor to the end of the previous line
+            cursorColumn = previousLine.length();
             cursorRow--;
+            // Update view
+            pcs.firePropertyChange("cursor", null, new CursorView.CursorDelta(-1, cursorColumn));
+            
+            // Add deleted line to the end of the line
+            previousLine.append(currentLine);
+            pcs.firePropertyChange("text", null, currentLine.toString());
         } else if (deletePosition == currentLine.length() && cursorRow < lines.size() - 1) {
-            currentLine.append(lines.get(cursorRow + 1));
+            // If delete is pressed at the end of the line
+            
+            // Get line to be removed
+            StringBuilder removeLine = lines.get(cursorRow + 1);
             lines.remove(cursorRow + 1);
+            // Update view
+            String[] movedLines = new String[lines.size() - cursorRow - 1];
+            for (int i = 0; i < movedLines.length; i++) {
+                movedLines[i] = lines.get(cursorRow + i + 1).toString();
+            }
+            pcs.firePropertyChange("text", null, "\n\u001b[J" + String.join("\n", movedLines));
+            
+            // Add removed line to the end of current line
+            currentLine.append(removeLine);
+            // Update view
+            pcs.firePropertyChange("text", null, removeLine.toString());
         }
     }
 
     public void lineJump() {
         // Get current line
         StringBuilder currentLine = lines.get(cursorRow);
+        String carriedText = currentLine.substring(cursorColumn);
+        
+        // Clear current line
+        currentLine.delete(cursorColumn, currentLine.length());
+        pcs.firePropertyChange("text", null, "\u001b[K");
+        
+        // Build new line
         StringBuilder newLine = new StringBuilder();
+        newLine.append(carriedText);
 
-        // Take text after cursor if there is
-        if (cursorColumn != currentLine.length()) {
-            newLine.append(currentLine.substring(cursorColumn));
-            currentLine.delete(cursorColumn, currentLine.length());
-        }
-
-        // Add new line and update cursor position
-        lines.add(cursorRow + 1, newLine);
+        // Move cursor to new line
+        int previousColumn = cursorColumn;
         cursorRow++;
         cursorColumn = 0;
+        pcs.firePropertyChange("cursor", null, new CursorView.CursorDelta(1, -previousColumn));
+        
+        // Add new line
+        lines.add(cursorRow, newLine);
+        
+        // Send changed lines to terminal
+        String[] movedLines = new String[lines.size() - cursorRow];
+        for (int i = 0; i < movedLines.length; i++) {
+            movedLines[i] = "\u001b[K" + lines.get(cursorRow + i).toString();
+        }
+        pcs.firePropertyChange("text", null, String.join("\n", movedLines));
     }
 
     public String getFinalString() {
