5d360f696e5188b7a81d8b11ba2a94097d53a7af
musergi <sergi.mus@gmail.com>
2020-03-16 09:44:48 +0100

    Added big fixed_multiline that enables scrolling

---
+++ practica1/fixed_multiline/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static void loadLibrary() {
+        String mainFolder = new File(Main.class.getResource("Main.class").getFile()).getParent();
+        System.load(mainFolder + "/libconsole_utils.so");
+        ConsoleUtils.init_console();
+    }
+    public static void main(String[] args) throws IOException {
+        loadLibrary();
+        System.out.println();
+        System.out.println("\n" + new MultilineReader(new InputStreamReader(System.in)).readLines());
+    }
+}
---
+++ practica1/fixed_multiline/src/MultilineReader.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class MultilineReader extends BufferedReader {
+    private SequenceParser parser;
+    private Multiline multiline;
+
+    public MultilineReader(Reader in) {
+        super(in);
+        parser = new SequenceParser(new BufferedReader(in));
+        multiline = new Multiline();
+    }
+    
+    public int read() throws IOException {
+        return parser.next();
+    }
+
+    public String readLines() throws IOException {
+        int keyCode;
+        ConsoleUtils.set_raw_mode();
+        while ((keyCode = read()) != SequenceParser.K_CTRL_D) {
+            multiline.process(keyCode);
+        }
+        return multiline.getFinalString();
+    }
+}
