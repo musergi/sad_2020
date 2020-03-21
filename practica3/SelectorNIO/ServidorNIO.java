3c71d41b954f207dd540df79b7a5373b03fce325
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-21 19:54:01 +0100

    Created practica3

---
+++ practica3/SelectorNIO/ServidorNIO.java
@@ -0,0 +1,8 @@
+/**
+ * Patron reactor: El patrón de diseño reactor es un patrón de programación concurrente 
+ * para manejar los pedidos de servicio entregados de forma concurrente a un manejador de 
+ * servicio desde una o más entradas. El manejador de servicio demultiplexa los pedidos entrantes 
+ * y los entrega de forma sincrónica a los manejadores de pedidos asociados.
+ * 
+ * NIO: Defines buffers, which are containers for data, and provides an overview of the other NIO packages.
+ */
---
+++ practica3/XatGrafic/AreaUsuaris.java
@@ -0,0 +1,5 @@
+/**
+ * Usar JList on el model l'hem de modificar per 
+ * a crear un widget amb forma MVC
+ * Model DefaultListModel com a model JList
+ */
