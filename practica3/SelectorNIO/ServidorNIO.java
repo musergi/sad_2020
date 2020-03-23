e3d8887021142ec5e84d9b4f75eb32572ee6adbc
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-23 09:14:23 +0100

    Hotfix

--- practica3/SelectorNIO/ServidorNIO.java
+++ practica3/SelectorNIO/ServidorNIO.java
@@ -1,4 +1,5 @@
 /**
+ * 
  * Patron reactor: El patrón de diseño reactor es un patrón de programación concurrente 
  * para manejar los pedidos de servicio entregados de forma concurrente a un manejador de 
  * servicio desde una o más entradas. El manejador de servicio demultiplexa los pedidos entrantes 
