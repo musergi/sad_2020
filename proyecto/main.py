0b4b8b4a4af3505fb838c5b09ed786ff75207e5e
musergi <sergi.mus@gmail.com>
2020-04-07 09:38:57 +0200

    Fixed client

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+import pickle
 from urllib import request, parse
 from threading import Thread
 from functools import partial
 
 
 COLORS = ['red', 'blue', 'cyan', 'green', 'yellow', 'orange', 'black']
+POLL_TIME = 5000
 
 
 class Canvas:
@@ -25,6 +27,8 @@ class Canvas:
         self.canvas.bind('<B1-Motion>', self.press_handler) # Set mouse movement and pressed callback
         self.canvas.bind('<Button-1>', self.press_handler) # Set mouse press
 
+        Thread(target= lambda: get_draw_data({'from': 'musergi', 'to': 'norma'}, root, self.on_get_draw_data), daemon=True).start()
+
     def clear(self, event):
         self.canvas.delete(tk.ALL)
 
@@ -37,21 +41,27 @@ class Canvas:
             'color': self.draw_color
         }), daemon=True).start()
 
-        tk.after(5000, self.get_draw_data("FROM musergi TO norma"))
-
     def change_color(self, color):
         self.draw_color = color
 
+    def on_get_draw_data(self, draw_data):
+        self.canvas.create_oval(draw_data.x - 10, draw_data.y - 10, draw_data.x + 10, draw_data.y + 10, fill=draw_data.color, outline='')
+        
 
 def send_draw_data(data_dict: dict):
     data = parse.urlencode(data_dict)
-    request.urlopen(f"http://localhost:6969/drawing?{data}")
+    request.urlopen(f"http://localhost:6969/drawing", data)
 
-def get_draw_data(params=None):
-    args = parse.urlencode(params)
-    response = request.urlopen(f"http://localhost:6969/drawing?{args}")
-    response.read()
-    
+
+def get_draw_data(params, root, drawing_callback):
+    while True:
+        # Sleep
+        args = parse.urlencode(params)
+        response = request.urlopen(f"http://localhost:6969/drawing?{args}")
+        drawing_data_object = pickle.loads(response.read())
+        root.after(0, lambda: drawing_callback(drawing_data_object))
+
+
 
 # Create window object
 root = tk.Tk()
